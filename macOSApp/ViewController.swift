d6bf834a3d016d637998f09fcedeb2fe7b3c4350
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-04-22 16:08:04 +0300

    Initial Commit

---
+++ macOSApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSApp
+//
+//  Created by Fotie M. Constant on 22/04/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
